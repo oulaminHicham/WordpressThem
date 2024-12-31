5f513f7e5dcf9395e59986aedd8d8c0c46204f24
Hicham <oulaminhicham019@gamil.com>
2024-12-31 18:13:42 +0000

    fix index php problem

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php get_header() ?>
+<div class="container ">
+	<div class="row">
+
+			<div class="col-sm-6">
+				<div class="main--post m-5">
+					<h3>our postes</h3>
+					<div class="my-2">
+						<span>hicham</span>
+						<span>4990/47/499</span>
+						<span>20 commet</span>
+					</div>
+					<img src="<?php echo  get_template_directory_uri().'./screenshot.png' ; ?>" class="img-fluid max-widh:100% height:auto">
+			</div>
+		</div>
+	</div>
+</div>
+
+
 
 <?php get_footer() ?>
