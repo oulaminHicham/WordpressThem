645e3ad0ebd4630680a2712699b699916907142e
Hicham <oulaminhicham019@gamil.com>
2025-01-03 19:06:02 +0000

    just for back up

---
+++ single.php
@@ -0,0 +1,77 @@
+<?php get_header() ?>
+<div class="container ">
+<?php
+	if(have_posts()){ // start the condition
+		while (have_posts()) { // start etirate posts if it exist
+			the_post();
+
+?>
+				<div class="main--pos single-postt m-3">
+				<div class="row">
+          <?php
+            if (is_single()) {
+                edit_post_link(
+                    __('Edit This Post'), // Link text
+                    '<p>',                // Before link
+                    '</p>'                // After link
+                );
+            }
+            ?>
+          <div class="col-sm-6">
+              <h3>
+                <a href="<?php the_permalink() ?>">
+                  <?php the_title() ?>
+                </a>
+              </h3>
+              <div class="my-2 d-flex">
+                <span class="post-auther mx-2">
+                  <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464l349.5 0c-8.9-63.3-63.3-112-129-112l-91.4 0c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3z"/></svg>
+                  <?php the_author_posts_link() ?>
+                </span>
+                <span class="post-date mx-2">
+                  <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L64 64C28.7 64 0 92.7 0 128l0 16 0 48L0 448c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-256 0-48 0-16c0-35.3-28.7-64-64-64l-40 0 0-40c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 40L152 64l0-40zM48 192l80 0 0 56-80 0 0-56zm0 104l80 0 0 64-80 0 0-64zm128 0l96 0 0 64-96 0 0-64zm144 0l80 0 0 64-80 0 0-64zm80-48l-80 0 0-56 80 0 0 56zm0 160l0 40c0 8.8-7.2 16-16 16l-64 0 0-56 80 0zm-128 0l0 56-96 0 0-56 96 0zm-144 0l0 56-64 0c-8.8 0-16-7.2-16-16l0-40 80 0zM272 248l-96 0 0-56 96 0 0 56z"/></svg>
+                   <div><?php  the_date('F j, Y'); ?></div>
+                 </span>
+                <span class="post-comments mx-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9l.3-.5z"/></svg>
+                  <?php comments_popup_link('0 comments' , 'one comment' , '% comments' , 'text-dark text-decoration-none' , 'coments disabled') ?>
+                </span>
+              </div>
+              <?php the_post_thumbnail("" , [
+                              "class"=>"img-fluid max-width:100% height:auto" ,
+                              "title"=>"post image"
+                ]) ?>
+          </div>
+          <div class="col-sm-6">
+            <p class="post-cotent my-3">
+              <?php the_content() ; ?>
+            </p>
+          </div>
+        </div>
+					<hr>
+					<p>
+						<?php the_category(' | ') ?>
+					</p>
+					<p class="post-tags">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 80L0 229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7L48 32C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+						<?php
+						 	if(has_tag()) {
+							    the_tags();
+							} else {
+							    echo "this post doesn't have tags" ;
+							}
+						 ?>
+					</p>
+		</div>
+<?php
+	} // end etirate posts is exist
+}// end of condition
+else {
+	echo "<div style='height:500px;'";
+		echo "<h3> you don't have any post </h3>";
+	echo "</div>";
+}
+?>
+</div>
+
+<?php get_footer() ?>
